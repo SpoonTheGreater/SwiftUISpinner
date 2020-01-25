f699362b06e2facf06807a8f3428ed30574c99de
James Sadlier <jamiesadlier@gmail.com>
2020-01-25 16:53:21 +0000

    Initial commit

--- Sources/SwiftUISpinner/SwiftUISpinner.swift
+++ Sources/SwiftUISpinner/SwiftUISpinner.swift
@@ -1,3 +1,20 @@
-struct SwiftUISpinner {
-    var text = "Hello, World!"
+import SwiftUI
+import UIKit
+
+@available(iOS 13.0, *)
+public struct SwiftUISpinner: UIViewRepresentable {
+  
+  public var animating: Bool
+  
+  public func makeUIView(context: Context) -> UIActivityIndicatorView {
+    UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+  }
+  
+  public func updateUIView(_ view: UIActivityIndicatorView, context: Context) {
+    if animating {
+      view.startAnimating()
+    } else {
+      view.stopAnimating()
+    }
+  }
 }
--- Tests/SwiftUISpinnerTests/SwiftUISpinnerTests.swift
+++ Tests/SwiftUISpinnerTests/SwiftUISpinnerTests.swift
@@ -2,14 +2,11 @@ import XCTest
 @testable import SwiftUISpinner
 
 final class SwiftUISpinnerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftUISpinner().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  func testExample() {
+    
+  }
+  
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
